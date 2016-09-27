57c2a45997a2dc2f4a63524242774f48d4668795
huangweii <huangweii@chanjet.com>
2016-09-27 20:02:03 +0800

    引用非AMD规范的js文件

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@
  * Created by 黄威 on 9/27/16.
  */
 
+require.config({
+    paths: {
+        myjquery: 'http://www.hbjltv.com/static/js/jquery-1.11.1'/*千万不要添加扩展名js*/
+    },
+    shim: {
+        myjquery: {
+            exports: '$'
+        }
+    },
+    urlArgs: "v=1.0"
+});
 /*依赖print模块*/
 require(['./print'], function (print) {
     'use strict';
--- js/print.js
+++ js/print.js
@@ -3,10 +3,13 @@
  */
 
 /*依赖const模块 const不小心和js的关键字冲突了所以命名为const1*/
-define(['./const'], function (const1) {
-    document.getElementById("myButton").onclick=function (event) {
-        alert("通过require js绑定了事件")
-    };
+define(['./const', 'myjquery'], function (const1, $222) {
+    var $myButton = $222('#myButton');
+    $myButton.click(function (event) {
+        console.log("通过require js绑定了事件");
+        console.log($myButton.val())
+    });
+
     return {
         printColor: function () {
             console.log(const1.color);
