e43aead0420dfe89daf313c7dc6d5861de858872
huangweii <huangweii@chanjet.com>
2016-09-27 19:19:04 +0800

    通过require js绑定了事件

--- js/print.js
+++ js/print.js
@@ -4,6 +4,9 @@
 
 /*依赖const模块 const不小心和js的关键字冲突了所以命名为const1*/
 define(['./const'], function (const1) {
+    document.getElementById("myButton").onclick=function (event) {
+        alert("通过require js绑定了事件")
+    };
     return {
         printColor: function () {
             console.log(const1.color);
