b98e3dfdf74264b0adcf0585e0e093bf0c212ece
huangweii <huangweii@chanjet.com>
2017-07-18 16:06:35 +0800

    使用路径别名

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@
  * Created by 黄威 on 9/27/16.
  */
 
+requirejs.config({
+    // baseUrl: '/js',
+
+    paths: {
+        'praa123': 'print'
+    }
+});
 /*依赖print模块*/
-require(['./print'], function (print) {
+require(['praa123'], function (print) {
     'use strict';
     print.printColor();
 });
